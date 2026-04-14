749fadc0e5fb2778c08a1f4068a5f061677d524a
Tony Bartenope <r1gger1@yahoo.com>
2026-04-14 17:59:23 -0500

    Switch auth to shared startinglinehq Supabase project

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://npzpracrghkabvjosvny.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wenByYWNyZ2hrYWJ2am9zdm55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxOTYyNjcsImV4cCI6MjA5MTc3MjI2N30.Ut3A65dvLdkX2EvhZmI9Y5NFPeFEe3-1nIdJ4cwi6dc';
+const SUPABASE_URL = 'https://bvuzrhmqcrepsevkdutt.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2dXpyaG1xY3JlcHNldmtkdXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDcwMDMsImV4cCI6MjA5MTc4MzAwM30.9Ll1c7_hIw5Czksc9h0Sd2Hhf_tOlSLmbdTv9x64Yrk';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
